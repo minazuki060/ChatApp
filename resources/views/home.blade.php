7b4083b4622989ea424cedba941c0a85b241e14b
minazuki060 <119671496+minazuki060@users.noreply.github.com>
2023-04-07 09:02:35 +0900

    homeのviewの作成

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<link rel="stylesheet" href="{{ asset('/css/home.css') }}">
+    <head>
+    <div class="head">
+        <h1>ChatApp</h1>
+
+    </head>
+
+    <main>
+        <div class="side">
+        <p>サイドバー</p>
+        </div> 
+
+        <div class="content">
+        <p>メインコンテンツ</p>
+        </div>
+    </main>
+
+</html>
