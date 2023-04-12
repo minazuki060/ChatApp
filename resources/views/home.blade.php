3b0e0e9467b15b77e78af9872ec57f77b3860ce0
minazuki060 <119671496+minazuki060@users.noreply.github.com>
2023-04-12 12:33:41 +0900

    sp用のレイアウトを作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <link rel="stylesheet" href="{{ asset('/css/home.css') }}">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <div class="pc">
     <head>
-    <div class="head">
-        <h1>ChatApp</h1>
-
+        <div class="head">
+         <h1>ChatApp</h1>
+        </div> 
     </head>
 
     <main>
         <div class="side">
-        <p>サイドバー</p>
+         <p>サイドバー</p>
         </div> 
 
         <div class="content">
-        <p>メインコンテンツ</p>
+         <p>メインコンテンツ</p>
         </div>
     </main>
+ </div>
 
+
+ <div class="sp">
+    <head>
+     <div class="sp-head">
+        <h1>ChatApp</h1>
+     </div>
+    </head>
+
+     <div class="sp-content">
+        <p>メインコンテンツ</p>
+     </div>
+ </div>
 </html>
