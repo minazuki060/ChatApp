cbb332e6b8ccf40eed2eff0ab55c27aedff8a1b2
minazuki060 <119671496+minazuki060@users.noreply.github.com>
2023-04-25 15:31:37 +0900

    認証済userはuser洗濯画面に表示しないように修正

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -14,8 +14,12 @@ class GroupController extends Controller
     public function create(Request $request)
     {
         
+        //認証済みユーザーのidを取得
+        $userId = Auth::id();
+
         // ユーザー一覧をページネートで取得
-        $users = User::paginate(20);
+        $users = User::where('id', '<>', $userId)
+                 ->paginate(20);
 
         // 検索フォームで入力された値を取得する
         $search = $request->input('search');
@@ -31,25 +35,27 @@ class GroupController extends Controller
 
             // 単語を半角スペースで区切り、配列にする（例："山田 翔" → ["山田", "翔"]）
             $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
-
-            //認証済みユーザーのidを取得
-            $userId = Auth::id();
             
             // 単語をループで回し、ユーザーネームと部分一致するものがあれば、$queryとして保持される
             foreach($wordArraySearched as $value) {
                 $query->where('name', 'like', '%'.$value.'%')
                     ->whereNotIn('id', [$userId]);
             }
+            // 検索フォームにキーワードが入力されていない場合
+        } else {
+            // ユーザー一覧をページネートで取得し、認証済みユーザーは除外する
+            $query->where('id', '<>', $userId);
         }
+        
             // 上記で取得した$queryをページネートにし、変数$usersに代入
             $users = $query->paginate(20);
 
-        // ビューにusersとsearchを変数として渡す
-        return view('group/create')
-            ->with([
-                'users' => $users,
-                'search' => $search,
-            ]);
+            // ビューにusersとsearchを変数として渡す
+            return view('group/create')
+                ->with([
+                    'users' => $users,
+                    'search' => $search,
+                ]);
     }
 
     //トークルーム作成処理の実装
