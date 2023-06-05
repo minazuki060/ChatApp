cf0d48cec8ec7afd487e34e57ff08fe29836f525
minazuki060 <119671496+minazuki060@users.noreply.github.com>
2023-06-05 15:52:45 +0900

    ログイン画面にregisterの項目を追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,8 +36,10 @@
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
+                        
                     </a>
                 @endif
+                <a href="{{ route('register') }}" class="btn btn-primary">or Register</a>
 
                 <x-button class="ml-4">
                     {{ __('Log in') }}
